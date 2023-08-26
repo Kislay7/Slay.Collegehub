8a981b9053ec4007e5ed76ca6b0dbc9d33384b27
Kislay7 <96987297+Kislay7@users.noreply.github.com>
2023-08-27 02:35:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,3 +36,38 @@ parallax_el.forEach((el) => {
     duration: 1.5,
  },
  "0.6");
+
+// Select the elements to animate
+const animatedElements = document.querySelectorAll("[data-animated='false']");
+
+// Set up the Intersection Observer
+const observerOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5, // Trigger animation when 50% of the element is visible
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const target = entry.target;
+      target.setAttribute("data-animated", "true"); // Mark as animated
+
+      // Add GSAP animation to reveal from the bottom
+      gsap.fromTo(
+        target,
+        { y: "100%", opacity: 0 },
+        { y: 0, opacity: 1, duration: 1, ease: "power2.out" }
+      );
+
+      // Stop observing the element once it's animated
+      observer.unobserve(target);
+    }
+  });
+}, observerOptions);
+
+// Start observing each animated element
+animatedElements.forEach((element) => {
+  observer.observe(element);
+});
+
