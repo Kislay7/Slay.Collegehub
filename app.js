fbc50d4557c1363d47c936ab11a4d294b2ed8375
Kislay7 <96987297+Kislay7@users.noreply.github.com>
2023-08-27 03:25:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const parallax_el = document.querySelectorAll(".parallax");
 const main = document.querySelector("main");
 
-let xValue = 0,
- yValue = 0 ;
-
- window.addEventListener("mousemove",(e)=> {
-xValue = e.clientX - window.innerWidth / 2;
-yValue = e.clientY - window.innerHeight / 2;
-parallax_el.forEach((el) => {
-    let speedx = el.dataset.speedx;
-    let speedy = el.dataset.speedy;
-
-    el.style.transform= `translateX(calc( 0% + ${-xValue * speedx
-    }px)) translateY(calc(0% + ${yValue * speedy}px))`;
-});
+// Calculate the intensity of the parallax effect based on screen width
+const getParallaxIntensity = () => {
+   const screenWidth = window.innerWidth;
+   return screenWidth < 725 ? 1.2 : 1; // Adjust the value as needed
+ };
+ 
+ // Handle both mousemove and touchmove events
+ const handleParallax = (x, y) => {
+   xValue = (x - window.innerWidth / 2) * getParallaxIntensity();
+   yValue = (y - window.innerHeight / 2) * getParallaxIntensity();
+ 
+   parallax_el.forEach((el) => {
+     let speedx = el.dataset.speedx;
+     let speedy = el.dataset.speedy;
+ 
+     el.style.transform = `translateX(calc( 0% + ${-xValue * speedx}px)) translateY(calc(0% + ${yValue * speedy}px))`;
+   });
+ };
+ 
+ // Mousemove event for desktop
+ window.addEventListener("mousemove", (e) => {
+   handleParallax(e.clientX, e.clientY);
+ });
+ 
+ // Touchmove event for mobile
+ window.addEventListener("touchmove", (e) => {
+   const touch = e.touches[0];
+   handleParallax(touch.clientX, touch.clientY);
  });
 
  if(window.innerwidth >= 725) {
